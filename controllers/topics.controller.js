b6d83a62c88bbfd4fa14090213b1b422b41ac0a8
zaw-tun <zaw.tun@hotmail.com>
2024-08-27 14:33:12 +0100

    completed task 3

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,7 +1,13 @@
-const { selectTopics } = require("../models/topics.model");
+const { selectTopics, selectApis } = require("../models/topics.model");
 
 exports.getTopics = (req, res, next) => {
   selectTopics().then((topics) => {
     res.status(200).send({ topics });
+  });
+};
+
+exports.getApis = (req, res, next) => {
+  selectApis().then((apis) => {
+    res.status(200).send(apis);
   });
 };
