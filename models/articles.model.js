0d669671dee707e198f44ad3299a2604efb5fd41
zaw-tun <zaw.tun@hotmail.com>
2024-08-29 13:02:49 +0100

    completed task 7-post-comments

--- models/articles.model.js
+++ models/articles.model.js
@@ -25,31 +25,44 @@ exports.selectArticles = () => {
     });
 };
 
-const checkArticleIdExists = (article_id) => {
+exports.selectCommentsByArticleId = (article_id) => {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
     .then((data) => {
-      return data.rows.length !== 0;
-    });
-};
-exports.selectCommentsByArticleId = (article_id) => {
-  return db
-    .query(
-      "SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;",
-      [article_id]
-    )
-    .then((data) => {
       if (data.rows.length === 0) {
-        return checkArticleIdExists(article_id).then((idExists) => {
-          if (idExists) {
-            return data.rows;
-          }
-          return Promise.reject({
-            status: 404,
-            message: "No such article_id yet",
-          });
+        return Promise.reject({
+          status: 404,
+          message: "No such article_id yet",
         });
       }
-      return data.rows;
+      return db
+        .query(
+          "SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;",
+          [article_id]
+        )
+        .then((data) => {
+          return data.rows;
+        });
+    });
+};
+
+exports.insertCommentById = (article_id, username, body) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .then((data) => {
+      if (data.rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          message: "No such article_id yet",
+        });
+      }
+      return db
+        .query(
+          "INSERT INTO comments (body, article_id, author) VALUES ($1, $2, $3) RETURNING *;",
+          [body, article_id, username]
+        )
+        .then((data) => {
+          return data.rows[0];
+        });
     });
 };
