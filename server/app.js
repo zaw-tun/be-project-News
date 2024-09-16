c2d5004672619cb22045bb21433265313ea1a77d
zaw-tun <zaw.tun@hotmail.com>
2024-09-16 10:54:00 +0100

    allow cross origin resource sharing

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
+app.use(cors());
 const fs = require("fs/promises");
 const { getTopics, getApis } = require("../controllers/topics.controller");
 const {
